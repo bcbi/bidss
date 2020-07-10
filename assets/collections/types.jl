f56fd58f02f7cb8cf0ae1387df1cf6ede7051a77
Dilum Aluthge <dilum@aluthge.com>
2020-07-10 19:06:55 -0400

    Cleanup

--- assets/collections/types.jl
+++
@@ -1,8 +0,0 @@
-# This file was generated, do not modify it. # hide
-# Define two variables x and y
-x = 100
-y = 3.14
-
-# Print out the variable types for each
-println(typeof(x))
-println(typeof(y))
--- assets/programming/dataframes/dataframes.jl
+++
@@ -1,3 +0,0 @@
-# This file was generated, do not modify it. # hide
-# print row 2
-println(df[2, :])
