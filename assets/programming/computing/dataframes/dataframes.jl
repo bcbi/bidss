ccfe263ba8d125b22e7bbaf5bb8bc438dfea093e
aeisman <aeisman@users.noreply.github.com>
2020-07-20 15:49:11 +0000

    Deploying to gh-pages from  @ d75d6a385dc2deac22c0179a325e198df47150f1 🚀

--- assets/programming/computing/dataframes/dataframes.jl
+++ assets/programming/computing/dataframes/dataframes.jl
@@ -1,3 +1,12 @@
 # This file was generated, do not modify it. # hide
-# print row 2
-println(df[2, :])
+# size of dataframe = size(df)
+# set number of rows to nrows and number of columns to ncols
+println("(nrows, ncols) = $(size(df))")
+nrows, ncols = size(df)
+
+# use nested for loop to get information from dataframe by row and column
+for row in 1:nrows
+  for col in 1:ncols
+	println("value for row $row and col $col is $(df[row,col])")
+  end
+end
